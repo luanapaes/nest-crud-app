1449ba6249f7dc6bd48c9d2a023876770bc2bf1c
Luana Paes <luanafranca598@gmail.com>
2024-08-26 23:28:39 -0300

    feat: adiciona controller e provider

--- src/todos/todos.module.ts
+++ src/todos/todos.module.ts
@@ -1,6 +1,8 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Todo } from "./todo.entity";
+import { TodosController } from "./todos.controller";
+import { TodosService } from "./todos.service";
 
 
 @Module({
@@ -8,7 +10,9 @@ import { Todo } from "./todo.entity";
         TypeOrmModule.forFeature(
             [Todo]
         )
-    ]
+    ],
+    controllers: [TodosController],
+    providers: [TodosService]
 })
 export class TodosModule{
 
