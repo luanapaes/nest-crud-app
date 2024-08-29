40c7b8fafbb040b5c3f639e7ac7942df8836c5ea
Luana Paes <luanafranca598@gmail.com>
2024-08-29 20:31:03 -0300

    refatorar (todos): atualiza TodosModule para integrar serviços de usuário e autenticação
    - Substituído `Todo` por `TodoEntity` e adicionado `UserEntity` ao TypeOrmModule para gerenciamento de recursos.
    - `UserModule` e `AuthModule` integrados com referências futuras para melhor gerenciamento de dependências.
    - Adicionados `AuthService` e `JwtService` aos provedores para lidar com operações relacionadas à autenticação.
    - Configurei `TodoIdCheckMiddleware` para validar parâmetros de requisição.

--- src/todos/todos.module.ts
+++ src/todos/todos.module.ts
@@ -1,19 +1,26 @@
-import { MiddlewareConsumer, Module, RequestMethod } from "@nestjs/common";
+import { forwardRef, MiddlewareConsumer, Module, RequestMethod } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { Todo } from "./todo.entity";
+import { TodoEntity } from "./todo.entity";
 import { TodosController } from "./todos.controller";
 import { TodosService } from "./todos.service";
 import { TodoIdCheckMiddleware } from "src/middlewares/todo-id-check.middleware";
+import { UserEntity } from "src/user/entity/user.entity";
+import { AuthModule } from "src/auth/auth.module";
+import { AuthService } from "src/auth/auth.service";
+import { JwtService } from "@nestjs/jwt";
+import { UserModule } from "src/user/user.module";
 
 
 @Module({
     imports: [
+        forwardRef(() => UserModule),
+        forwardRef(() => AuthModule),
         TypeOrmModule.forFeature(
-            [Todo]
+            [TodoEntity, UserEntity]
         )
     ],
     controllers: [TodosController],
-    providers: [TodosService]
+    providers: [TodosService, TodoEntity, AuthService, JwtService]
 })
 export class TodosModule{
 
