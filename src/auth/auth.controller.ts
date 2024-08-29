932bb1af70e0a80b798c0dc8c45a90feeb9c9e51
Luana Paes <luanafranca598@gmail.com>
2024-08-29 20:12:46 -0300

    feat(auth): implementa AuthController para endpoints de autenticação de usuário
    - Adiciona endpoints para login, registro e redefinição de senha.
    - AuthService integrado para lidar com a lógica de autenticação.
    - Define DTOs para solicitações de login, cadastro e redefinição de senha.

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { AuthLoginDTO } from "./dto/auth-login.dto";
+import { AuthRegisterDTO } from "./dto/auth-register.dto";
+import { AuthResetDTO } from "./dto/auth-reset.dto";
+
+@Controller('auth')
+export class AuthController{
+    constructor(
+        private readonly authService: AuthService
+    ){}
+
+    @Post('login')
+    async login(@Body() {email, password}: AuthLoginDTO){
+        return this.authService.login(email, password);
+    }
+
+    @Post('register')
+    async register(@Body() body: AuthRegisterDTO){
+        return this.authService.register(body)
+    }
+
+    @Post('reset')
+    async reset(@Body() {password, token}: AuthResetDTO){
+        return this.authService.reset(password, token);
+    }
+
+    
+}
