4fe50d0cfe3cc8557458d5b06b534340fc38fd4c
Luana Paes <luanafranca598@gmail.com>
2024-08-29 21:17:05 -0300

    feat(user): Adiciona UserController com operações CRUD

---
+++ src/user/user.controller.ts
@@ -0,0 +1,44 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, UseGuards } from "@nestjs/common";
+import { CreateUserDTO } from "./dto/create-user.dto";
+import { UpdatePatchUserDTO } from "./dto/update-patch-user.dto";
+import { UpdatePutUserDTO } from "./dto/update-put-user.dto";
+import { UserService } from "./user.service";
+import { AuthGuard } from "src/guards/auth.guard";
+
+@UseGuards(AuthGuard)
+@Controller('users')
+export class UserController{
+    
+    constructor(private readonly userService: UserService) { }
+
+    @Post()
+    async create(@Body() data: CreateUserDTO) {
+        return this.userService.create(data)
+    }
+
+    @Get()
+    async read() {
+        return this.userService.list()
+    }
+
+    @Get(':id')
+    async readOne(@Param("id") id: number) {
+        return this.userService.listOne(id)
+    }
+
+    @Put(':id')
+    async update(@Body() data: UpdatePutUserDTO, @Param("id") id: number) {
+        return this.userService.update(id, data)
+    }
+
+    @Patch(':id')
+    async updatePartial(@Body() data: UpdatePatchUserDTO, @Param("id") id: number) {
+        return this.userService.updatePartial(id, data)
+    }
+
+    @Delete(':id')
+    async delete(@Param("id") id: number) {
+        return this.userService.delete(id)
+    }
+
+}
