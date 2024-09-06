767b94d66f399d9956e413ff279dcf839087981a
Luana Paes <luanafranca598@gmail.com>
2024-09-05 21:00:49 -0300

    feat(middleware): adiciona MarcaIdCheckMiddleware para validar o ID do usuário nas solicitações

---
+++ src/middlewares/marca-id-check-middleware.ts
@@ -0,0 +1,15 @@
+import { BadRequestException, NestMiddleware } from "@nestjs/common";
+import { NextFunction, Request, Response } from "express";
+
+
+export class MarcaIdCheckMiddleware implements NestMiddleware {
+    use(req: Request, res: Response, next: NextFunction) {
+        if (isNaN(Number(req.params.id)) || Number(req.params.id) <= 0) {
+
+            throw new BadRequestException("Id inválido")
+        }
+
+        next();
+    }
+
+}
